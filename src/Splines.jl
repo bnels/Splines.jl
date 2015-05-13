a12085c0b17ed9b2fa334b10ee94dc93d7607671
Brad Nelson <bjnelson@stanford.edu>
2015-05-13 11:24:33 -0700

    vectorized BSplineSolve

--- src/Splines.jl
+++ src/Splines.jl
@@ -39,7 +39,7 @@ Spline{T}(s::Spline{T}) = Spline{T}(copy(s.t), copy(s.u))
 #    return i
 #end
 
-# Evaluate Spline Basis defined by knot sequence t at point x
+# Evaluate BSpline Basis defined by knot sequence t at point x
 function BSplineEval(t::Vector{Float64}, ord::Int, knot::Int, x::Float64)
     B = zeros(ord+1, ord+1)
     for i = 1:ord+1
@@ -47,14 +47,28 @@ function BSplineEval(t::Vector{Float64}, ord::Int, knot::Int, x::Float64)
     end
     for k = 1:ord
         for i = 1:1+ord-k
-            c1 = (x - t[knot + i - 1]) / (t[knot + i + k - 1] - t[knot + i - 1])
-            c2 = (t[knot + i + k] - x) / (t[knot + i + k] - t[knot + i])
+            r1 = (t[knot + i + k - 1] - t[knot + i - 1])
+            r2 = (t[knot + i + k] - t[knot + i])
+            r1 == 0 ? c1 = 0 : c1 = (x - t[knot + i - 1]) / r1
+            r2 == 0 ? c2 = 0 : c2 = (t[knot + i + k] - x) / r2
             B[i,k+1] = c1*B[i,k] + c2*B[i+1,k]
         end
     end
     return B[1,ord+1]
 end
 
+function BSplineEval(t::Vector{Float64}, ord::Int, knot::Int, x::Vector{Float64})
+    n = size(x,1)
+    f = zeros(n)
+    for i = 1:n
+        f[i] = BSplineEval(t, ord, knot, x[i])
+    end
+    return f
+end
+
+# create matrix that has BSpline basis function value evaluated at each knot
+function SplineMatrix(t::Vector{Float64}, ord::Int)
+end
 
 #function DeBoor(s::Spline, ord::Int, x::Float64, k::Int, i::Int)
 #    α = (x - s.u[i]) / (s.u[i + ord + 1 - k] - s.u[i])
